2121b3bfd0553e8680002f169160a9dc759add1f
renrizzolo <renrizmusic@gmail.com>
2021-01-03 12:52:59 +1100

    theme config

--- gatsby-theme-golden-condor/gatsby-config.js
+++ gatsby-theme-golden-condor/gatsby-config.js
@@ -1,11 +1,33 @@
 module.exports = ({ contentPath = "data" }) => ({
+  siteMetadata: {
+    title: `Blog Title Placeholder`,
+    author: `Name Placeholder`,
+    description: `Description placeholder`,
+    siteUrl: `https://example.com`,
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
   plugins: [
-    "gatsby-plugin-theme-ui"
+    {
+      resolve: "gatsby-plugin-theme-ui",
+      options: {
+        // prismPreset: 'night-owl',
+        // preset: "@theme-ui/preset-swiss",
+      },
+    },
     // {
     //   resolve: "gatsby-source-filesystem",
     //   options: {
     //     path: contentPath
     //   }
     // }
-  ]
+  ],
 });
