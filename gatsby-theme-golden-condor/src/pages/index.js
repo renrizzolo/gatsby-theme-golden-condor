d2c6940e95411cd2c9bdbf7c1f3420a0ff394ae7
renrizzolo <renrizmusic@gmail.com>
2021-01-19 14:27:33 +1100

    generic page

--- gatsby-theme-golden-condor/src/pages/index.js
+++ gatsby-theme-golden-condor/src/pages/index.js
@@ -1,9 +1,64 @@
+import PostCard from "@components/PostCard";
 import React from "react";
-import Layout from "../components/Layout";
-import { Text } from "theme-ui";
+import { Container, Flex, Heading, Box, Grid, Badge, Divider } from "theme-ui";
+import { Link } from "@components/UI";
+import { graphql } from "gatsby";
+import Layout from "@components/Layout";
 
-export default () => (
-  <Layout>
-    <Text>Home page.</Text>
-  </Layout>
+export default ({ data }) => (
+  <>
+    <Layout>
+      {data.page && (
+        <Box as="section">
+          <div dangerouslySetInnerHTML={{ __html: data.page.content }} />
+        </Box>
+      )}
+      <Box p={4}>
+        <Container as="section">
+          <Box pb={5} />
+
+          <Heading variant="jumbo">Latest Posts</Heading>
+        </Container>
+      </Box>
+      <Box p={4}>
+        <Container as="section">
+          {/* <Divider /> */}
+
+          <Grid gap={4} columns={[1, 2, 3]}>
+            {data.posts.nodes.map((post, i) => (
+              <PostCard post={post} />
+            ))}
+          </Grid>
+          <Box pb={5} />
+          <Heading variant="jumbo">Concrete Playground</Heading>
+        </Container>
+      </Box>
+    </Layout>
+  </>
 );
+
+export const query = graphql`
+  {
+    page(title: { eq: "Home" }) {
+      content
+    }
+    posts: allMdx {
+      nodes {
+        slug
+        frontmatter {
+          date(formatString: "MMMM Do, YYYY")
+          title
+          category
+          excerpt
+          image {
+            childImageSharp {
+              fluid(maxWidth: 400, maxHeight: 225) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
