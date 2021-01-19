4b690b250e47cf520ca48b9c580d519d8db77e29
renrizzolo <renrizmusic@gmail.com>
2021-01-19 14:30:03 +1100

    _

--- gatsby-theme-golden-condor/src/pages/about/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Layout from "../../components/Layout";
-import { Text } from "theme-ui";
-
-export default () => (
-  <Layout>
-    <Text>About.</Text>
-  </Layout>
-);
