8f946fa2f145f8d15c5edeb154cd40eae366b412
renrizzolo <renrizmusic@gmail.com>
2021-01-19 14:25:46 +1100

    example/test site

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  plugins: [{ resolve: `gatsby-theme-golden-condor`, options: {} }],
+  
+  plugins: [{
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `gatsby-theme-golden-condor`,
+      short_name: `Gatsby Theme with theme-ui and MDX`,
+      start_url: `/`,
+      background_color: "#fff",
+      theme_color: "#1e1c14",
+      display: `minimal-ui`,
+      icon: `${__dirname}/src/images/theme-icon.png`, // This path is relative to the root of the site.
+    },
+  },{ resolve: `gatsby-theme-golden-condor`, options: {} }],
+
 };
--- example/src/pages/index.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export default () => <div>Example overriding home page.</div>;
