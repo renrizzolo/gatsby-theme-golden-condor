e83e886bfeae2d413bc4591912560a38dd63cca0
renrizzolo <renrizmusic@gmail.com>
2021-04-13 22:41:06 +1000

    test shadowing theme

---
+++ example/src/gatsby-theme-golden-condor/theme/index.js
@@ -0,0 +1,12 @@
+import theme from "gatsby-theme-golden-condor/src/theme/index";
+
+// const myTheme = {
+//   ...theme,
+//   colors: {
+//     ...theme.colors,
+//     primary: ["yellow", "orange", "tomato", "crimson", "black"],
+//     secondary: ["blue", "acqua", "aliceblue"],
+//     muted: "brown",
+//   },
+// };
+export default theme;
